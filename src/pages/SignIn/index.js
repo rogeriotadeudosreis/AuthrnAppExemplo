339062fa5953799d539908606584a563f45dc6f2
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-09-24 17:32:02 -0300

    estilizacao

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -25,7 +25,7 @@ export function SignIn() {
     signIn(usuario);
   }
 
-  function handleSignOut(){
+  function handleSignOut() {
     singOut();
   }
 
--- src/pages/Tags/index.js
+++ src/pages/Tags/index.js
@@ -1,71 +1,76 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet, FlatList, Alert } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet, FlatList, Alert} from 'react-native';
+import Footer from '../../components/footer';
+import Header from '../../components/header';
 
-import { useAuth } from '../../contexts/auth';
-import { listarTags, deletar } from '../../services/tagService';
+import {useAuth} from '../../contexts/auth';
+import {listarTags, deletar} from '../../services/tagService';
 
-import { Elogio } from './Elogio';
+import {Elogio} from './Elogio';
 import styles from './styles';
-
-
 
 export default function Tags() {
   //OBTENDO OS DADOS DO USECONTEXT AUTH
-  const { token, signOut } = useAuth();
+  const {token, signOut} = useAuth();
   const [listagem, setListagem] = useState();
-
 
   function handleSignOut() {
     //MÉTODO PASSADO PELO USECONTEXT
     signOut();
   }
 
-
   async function handleListarTags() {
     try {
       //MÉTODO VEM DO TAGSERVICES
       setListagem(await listarTags());
-      console.log('dentro dashboard' + listagem)
-
+      console.log('dentro dashboard' + listagem);
     } catch (error) {
       //FAÇA O TRATAMENTO DE ERROS, USANDO ALGUM COMPONENTE OU O PRÓPRIO ALERT
-      alert(error)
+      alert(error);
     }
-
   }
 
   async function handleDeletar(idSelecao) {
     try {
       //MÉTODO VEM DO TAGSERVICES
-      await deletar(idSelecao)
+      await deletar(idSelecao);
       //APAGANDO DA LISTA O ITEM
-      setListagem(vetorVelho => vetorVelho.filter(elogio => elogio.id !== idSelecao))
-      console.log('dentro dashboard' + listagem)
+      setListagem((vetorVelho) =>
+        vetorVelho.filter((elogio) => elogio.id !== idSelecao),
+      );
+      console.log('dentro dashboard' + listagem);
     } catch (error) {
-      alert(error)
+      alert(error);
     }
-
   }
 
-
   return (
-    <View style={{backgroundColor:'#000', flex:1}}>
-      <View style={styles.containerBotoes}>
-        <Button title="Listar Tags" color='green' onPress={handleListarTags} />
-        <Button title="Sair" color='#506d84' onPress={handleSignOut} />
-      </View>
-
-      {listagem &&
-        <View>
-          <Text style={styles.tituloListaElogios}>Listagem de Elogios</Text>
-          <FlatList
-            data={listagem} keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => <Elogio titulo={item} onPress={() => handleDeletar(item.id)} />}
+    <>
+    <Header/>
+      <View style={{backgroundColor: '#2c3e50', flex: 1}}>
+        <View style={styles.containerBotoes}>
+          <Button
+            title="Listar Tags"
+            color="green"
+            onPress={handleListarTags}
           />
+          <Button title="Sair" color="#506d84" onPress={handleSignOut} />
         </View>
-      }
 
-    </View>
+        {listagem && (
+          <View>
+            <Text style={styles.tituloListaElogios}>Listagem de Elogios</Text>
+            <FlatList
+              data={listagem}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({item}) => (
+                <Elogio titulo={item} onPress={() => handleDeletar(item.id)} />
+              )}
+            />
+          </View>
+        )}
+      </View>
+      <Footer />
+    </>
   );
-};
-
+}
